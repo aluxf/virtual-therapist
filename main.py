0b29c9a708bea38d58406d8d749e570cf4a9d11e
maha8298 <55284828+maha8298@users.noreply.github.com>
2023-12-21 10:42:52 +0100

    Added all questions with different answer alternatives

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
     questions = [
         "How would you rate your overall well-being as a student?",
         "On a scale of 1 to 5, how would you rate your stress levels on average?",
-        # "Have you experienced any significant changes in your mental health while being a student?",
-        # "How do you feel that your mental health is supported and prioritized by your educational institution?",
-        # "How often do you feel overwhelmed by academic responsibilities and deadlines?",
-        # "Have you experienced any symptoms of anxiety, such as excessive worry or restlessness?",
-        # "How much sleep are you getting on a regular basis?",
-        # "Do you engage in regular physical activity and maintain a healthy lifestyle?",
-        # "How would you rate your school-life balance?",
+        "Have you experienced any significant changes in your mental health while being a student?",
+        "How do you feel that your mental health is supported and prioritized by your educational institution?",
+        "How often do you feel overwhelmed by academic responsibilities and deadlines?",
+        "Have you experienced any symptoms of anxiety, such as excessive worry or restlessness?",
+        "How much sleep are you getting on a regular basis?",
+        "Do you engage in regular physical activity and maintain a healthy lifestyle?",
+        "How would you rate your school-life balance?",
     ]
 
     question_set = [
@@ -21,7 +21,35 @@ if __name__ == "__main__":
             ["horrible", "bad", "okay", "good", "excellent"],
         ),
         (
-            "On a scale of 1 to 5, how would you rate your stress levels on average?",
+            "On a scale from 1 to 5, how would you rate your stress levels on average?",
+            ["5", "4", "3", "2", "1"],
+        ),
+        (
+            "Have you experienced any significant changes in your mental health while being a student?",
+            ["many", "few", "neutral", "some", "none"],
+        ),
+        (
+            "How do you feel that your mental health is supported and prioritized by your educational institution?",
+            ["horrible", "bad", "neutral", "good", "excellent"],
+        ),
+        (
+            "How often do you feel overwhelmed by academic responsibilities and deadlines?",
+            ["always", "often", "neutral", "sometimes", "never"],
+        ),
+        (
+            "Have you experienced any symptoms of anxiety, such as excessive worry or restlessness?",
+            ["many", "few", "neutral", "some", "none"],
+        ),
+        (
+            "How much sleep are you getting on a regular basis? On a scale from 1 to 5.",
+            ["1", "2", "3", "4", "5"],
+        ),
+        (
+            "Do you engage in regular physical activity and maintain a healthy lifestyle?",
+            ["never", "sometimes", "neutral", "regularly", "frequently"],
+        ),
+        (
+            "On a scale from 1 to 5, how would you rate your school-life balance?",
             ["1", "2", "3", "4", "5"],
         ),
     ]
